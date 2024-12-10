dca38fa61cc8e9c01d4e5b0a465f5ee7716e85cb
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-10 20:17:11 +0000

    one more nav

--- anuya/src/App.js
+++ anuya/src/App.js
@@ -1,16 +1,53 @@
 import React from "react";
-import './App.css';
-import Header from './components/header';
-import Hero from './components/hero';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Navbar from "./components/NavBar"; // Ensure Navbar is imported
+import "bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./components/gallery";
+import Original from "./components/original";
+import PrintStudio from "./components/printstudio";
+import NFT from "./components/nft";
+import HomeDecor from "./components/interior";
+import SoldArt from "./components/soldart";
+import Exhib from "./components/exhib";
+import MakeArt from "./components/makeart";
+import ClassOnline from "./components/classonline";
+import ClassIRL from "./components/classirl";
+import More from "./components/more";
+import About from "./components/about";
+import FAQ from "./components/faq";
+import Contact from "./components/contact";
+import NotFound from "./components/404"; // Renamed 404 for clarity
 
 function App() {
   return (
-    <div className="App">
-            <Header />
-      <Hero />
-    </div>
+    <Router>
+      <Header />
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/original" element={<Original />} />
+          <Route path="/printstudio" element={<PrintStudio />} />
+          <Route path="/nft" element={<NFT />} />
+          <Route path="/homedecor" element={<HomeDecor />} />
+          <Route path="/soldart" element={<SoldArt />} />
+          <Route path="/exhib" element={<Exhib />} />
+          <Route path="/makeart" element={<MakeArt />} />
+          <Route path="/classonline" element={<ClassOnline />} />
+          <Route path="/classirl" element={<ClassIRL />} />
+          <Route path="/more" element={<More />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Hero />
+      </div>
+    </Router>
   );
 }
 
--- anuya/src/components/NavBar.js
+++ anuya/src/components/NavBar.js
@@ -1,68 +1,55 @@
-import React, { useState } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  Nav,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from 'reactstrap';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import "../App.css";
 
-function NavBar() {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
+const Navbar = () => {
   return (
-    <div>
-      <Navbar color="faded" light expand="md">
-        <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            {/* Gallery Dropdown */}
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Gallery
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem href="/components/forsale">Art for Sale</DropdownItem>
-                <DropdownItem href="/components/originals">Originals</DropdownItem>
-                <DropdownItem href="/components/printstudio">Fine Art Prints</DropdownItem>
-                <DropdownItem href="/components/nft">Digital Art</DropdownItem>
-                <DropdownItem href="/components/interior">Interior</DropdownItem>
-                <DropdownItem href="/components/soldart">Sold Art</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            {/* Make Art Dropdown */}
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Make Art
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem href="/components/classonline">Online Class</DropdownItem>
-                <DropdownItem href="/components/classirl">Make Art IRL</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            {/* Read More Dropdown */}
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Read More
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem href="/components/camilla">Camilla</DropdownItem>
-                <DropdownItem href="/components/faq">FAQ</DropdownItem>
-                <DropdownItem href="/components/exhib">Exhibitions</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+    <nav className="nav__container">
+      <div className="nav__menu" id="nav-menu">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink to="/" className="nav__link">Home</NavLink>
+          </li>
+          <li className="nav__item dropdown">
+            <span className="nav__link">Gallery</span>
+            <ul className="dropdown__menu">
+              <li><NavLink to="/gallery" className="dropdown__link">Art for Sale</NavLink></li>
+              <li><NavLink to="/original" className="dropdown__link">Originals</NavLink></li>
+              <li><NavLink to="/printstudio" className="dropdown__link">Fine Art Prints</NavLink></li>
+              <li><NavLink to="/nft" className="dropdown__link">Digital Art</NavLink></li>
+              <li><NavLink to="/homedecor" className="dropdown__link">Interior</NavLink></li>
+              <li><NavLink to="/soldart" className="dropdown__link">Sold Art</NavLink></li>
+            </ul>
+          </li>
+          <li className="nav__item dropdown">
+            <span className="nav__link">Make Art</span>
+            <ul className="dropdown__menu">
+              <li><NavLink to="/classonline" className="dropdown__link">Online Class</NavLink></li>
+              <li><NavLink to="/classirl" className="dropdown__link">Make Art IRL</NavLink></li>
+            </ul>
+          </li>
+          <li className="nav__item dropdown">
+            <span className="nav__link">Read More</span>
+            <ul className="dropdown__menu">
+              <li><NavLink to="/about" className="dropdown__link">Camilla</NavLink></li>
+              <li><NavLink to="/faq" className="dropdown__link">FAQ</NavLink></li>
+              <li><NavLink to="/exhib" className="dropdown__link">Exhibitions</NavLink></li>
+            </ul>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/contact" className="nav__link">Contact</NavLink>
+          </li>
+        </ul>
+        <div className="nav__close" id="nav-close">
+          <IoClose />
+        </div>
+      </div>
+      <div className="nav__toggle" id="nav-toggle">
+        <IoMenu />
+      </div>
+    </nav>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;

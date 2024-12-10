b92787bfed844e05814957da87466a500a0e18cc
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-10 16:54:10 +0000

    nvbar toggle

--- anuya/src/components/NavBar.js
+++ anuya/src/components/NavBar.js
@@ -1,30 +1,35 @@
-const NavBar = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => setIsOpen(!isOpen);
 
   return (
-      <div>
-          <Navbar color="faded" light>
-              <NavbarBrand href="/" className="me-auto">
-                  reactstrap
-              </NavbarBrand>
-              <NavbarToggler onClick={toggleNavbar} className="me-2" />
-              <Collapse isOpen={!collapsed} navbar>
-                  <Nav navbar>
-                      <NavItem>
-                          <NavLink href="/components/">Components</NavLink>
-                      </NavItem>
-                      <NavItem>
-                          <NavLink href="https://github.com/reactstrap/reactstrap">
-                              GitHub
-                          </NavLink>
-                      </NavItem>
-                  </Nav>
-              </Collapse>
-          </Navbar>
-      </div>
+    <Navbar expand="md" color="light" light className="w-100">
+      <NavbarBrand href="/">MyBrand</NavbarBrand>
+      <NavbarToggler onClick={toggleNavbar} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
-};
+}
 
 export default NavBar;
--- anuya/src/components/header.js
+++ anuya/src/components/header.js
@@ -1,46 +1,45 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../images/logo/newtest.png";
-import NavBar from '../components/NavBar'; // Path and naming should match
+import NavBar from '../components/NavBar';
 import '../App.css';
 
 function Header() {
-    useEffect(() => {
-        const header = document.getElementById("stickyheader");
-        const sticky = header.offsetTop;
+  useEffect(() => {
+    const header = document.getElementById("stickyheader");
+    const sticky = header.offsetTop;
 
-        const handleScroll = () => {
-            if (window.pageYOffset > sticky) {
-                header.classList.add("sticky");
-            } else {
-                header.classList.remove("sticky");
-            }
-        };
+    const handleScroll = () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        // Cleanup on component unmount
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return (
-        <header className="header" id="stickyheader">
-            <div className="container-fluid">
-                <div className="row align-items-center">
-                    <div className="logo col-sm-4">
-                        <Link to="/">
-                            <img src={logo} className="logotype" alt="logo Art by Anuya" />
-                        </Link>
-                    </div>
-                    <div className="navbar col-sm-8">
-            <NavBar /> {/* Correctly rendered as a React component */}
-            </div>
-                                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header" id="stickyheader">
+      <div className="container-fluid">
+        <div className="row align-items-center">
+          <div className="col-4 d-flex align-items-center">
+            <Link to="/" className="d-flex align-items-center">
+              <img src={logo} className="logotype" alt="logo Art by Anuya" />
+            </Link>
+          </div>
+          <div className="col-8 text-end">
+            <NavBar /> {/* Include the NavBar component */}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

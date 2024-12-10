cdb449b8c3d3927cf60476c910c142ce88869597
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-10 18:24:42 +0000

    nav

--- anuya/src/components/NavBar.js
+++ anuya/src/components/NavBar.js
@@ -3,31 +3,61 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
-  NavItem,
-  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
 } from 'reactstrap';
 
-function NavBar(props) {
+function NavBar() {
   const [collapsed, setCollapsed] = useState(true);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   return (
     <div>
-      <Navbar color="faded" light>
-    <NavbarToggler onClick={toggleNavbar} className="me-2" />
+      <Navbar color="faded" light expand="md">
+        <NavbarToggler onClick={toggleNavbar} className="me-2" />
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
+            {/* Gallery Dropdown */}
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Gallery
+              </DropdownToggle>
+              <DropdownMenu end>
+                <DropdownItem href="/components/forsale">Art for Sale</DropdownItem>
+                <DropdownItem href="/components/originals">Originals</DropdownItem>
+                <DropdownItem href="/components/printstudio">Fine Art Prints</DropdownItem>
+                <DropdownItem href="/components/nft">Digital Art</DropdownItem>
+                <DropdownItem href="/components/interior">Interior</DropdownItem>
+                <DropdownItem href="/components/soldart">Sold Art</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+
+            {/* Make Art Dropdown */}
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Make Art
+              </DropdownToggle>
+              <DropdownMenu end>
+                <DropdownItem href="/components/classonline">Online Class</DropdownItem>
+                <DropdownItem href="/components/classirl">Make Art IRL</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+
+            {/* Read More Dropdown */}
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Read More
+              </DropdownToggle>
+              <DropdownMenu end>
+                <DropdownItem href="/components/camilla">Camilla</DropdownItem>
+                <DropdownItem href="/components/faq">FAQ</DropdownItem>
+                <DropdownItem href="/components/exhib">Exhibitions</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
         </Collapse>
       </Navbar>

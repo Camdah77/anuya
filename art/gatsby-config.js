984ff72872ddb95db1740e9b5b4dac8e802cc114
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-12 20:32:22 +0000

    gatsby-config.js.

--- art/gatsby-config.js
+++ art/gatsby-config.js
@@ -13,8 +13,10 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/src/blog`,
+        path: `${__dirname}/src/posts`,
       }
     },
+    `gatsby-plugin-mdx`,
+    `@mdx-js/react`, // Optional if you're using MDX components in React
   ],
 };

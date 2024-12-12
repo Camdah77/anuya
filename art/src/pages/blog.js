d31cbda192c722c7f57fd5f984976920b4c4a96c
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-12 19:35:47 +0000

    gatsby part4

--- art/src/pages/blog.js
+++ art/src/pages/blog.js
@@ -2,16 +2,34 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import Hero from "../components/hero2"
+import { graphql } from 'gatsby'
 
-const BlogPage = () => {
-    return (
+const BlogPage = ({ data }) => { 
+       return (
       <Layout pageTitle="News">
         <Hero />
-         <p>News, classes and more</p>
+        <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
       </Layout>
     )
   }
+  export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
   
-  export const Head = () => <Seo title="News: Art by Anuya | Camilla Dahlström" />
+export const Head = ({ data }) => <Seo title="News: Art by Anuya | Camilla Dahlström" />
   
   export default BlogPage

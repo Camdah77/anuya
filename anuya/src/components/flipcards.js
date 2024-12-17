4cbbad804a41015dbc9411a624bf88c2cb030b7f
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-17 14:01:12 +0000

    flipard work

--- anuya/src/components/flipcards.js
+++ anuya/src/components/flipcards.js
@@ -3,51 +3,80 @@ import ReactCardFlip from "react-card-flip";
 import "../styles/flipcards.css";
 
 const Flipcard = () => {
-  // State to handle flips for three cards
-  const [flipStates, setFlipStates] = useState([false, false, false]);
+  // State to handle flips for cards
+  const [hoverStates, setHoverStates] = useState([false, false, false, false, false, false]);
 
-  // Handle flip for individual cards
-  const handleFlip = (index) => {
-    setFlipStates((prevStates) =>
-      prevStates.map((state, i) => (i === index ? !state : state))
+  // Image URLs and headings for the flipcards
+  const cards = [
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip1.png",
+      text: "Original Art",
+    },
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip2.png",
+      text: "Printstudio",
+    },
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip3.png",
+      text: "Interior",
+    },
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip4.png",
+      text: "Camilla - My Story",
+    },
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip8.png",
+      text: "Sold Art",
+    },
+    {
+      image: "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip6.png",
+      text: "Exhibitions",
+    },
+  ];
+
+  // Handle hover states
+  const handleMouseEnter = (index) => {
+    setHoverStates((prev) =>
+      prev.map((state, i) => (i === index ? true : state))
     );
   };
 
-  // Image URLs for the flipcards
-  const frontImages = [
-    "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip1.png",
-    "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip2.png",
-    "https://res.cloudinary.com/dj9sie6nl/image/upload/v1734428380/Anuya/menycards/flip3.png",
-  ];
+  const handleMouseLeave = (index) => {
+    setHoverStates((prev) =>
+      prev.map((state, i) => (i === index ? false : state))
+    );
+  };
 
   return (
     <div className="flipcard-container">
-      {frontImages.map((image, index) => (
+      {cards.map((card, index) => (
         <ReactCardFlip
           key={index}
-          isFlipped={flipStates[index]}
+          isFlipped={hoverStates[index]}
           flipDirection="horizontal"
         >
-          {/* Front side */}
+          {/* Front Side */}
           <div
             className="flipcard"
-            onClick={() => handleFlip(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
           >
+            <div className="flipcard-heading">{card.text}</div>
             <img
-              src={image}
-              alt={`Gallery artwork ${index + 1}`}
+              src={card.image}
+              alt={card.text}
               className="flipcard-img"
             />
           </div>
 
-          {/* Back side */}
+          {/* Back Side */}
           <div
             className="flipcard flipcard-back"
-            onClick={() => handleFlip(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
           >
             <p className="flipcard-text">
-              Original artwork available.  
-              <br /> Click to flip back.
+              {card.text} - Explore More
             </p>
           </div>
         </ReactCardFlip>

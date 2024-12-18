040c8b4f8656bff4ca5f69484a117c39f1ee4de8
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2024-12-18 17:29:20 +0000

    started camilla site

--- anuya/src/components/camilla.js
+++ anuya/src/components/camilla.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import "../styles/camilla.css";
 
-
-function Camilla() {
-    return (
-      
-      <div className="container">
-      <div className="cdimage">
+const Camilla = () => {
+  return (
+    <div>
       <div className="app-heading">This is my story</div>
-        <img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1730892347/Anuya/Camilla.png" alt="Camilla"  />;
+      <div className="container">
+        <div className="row">
+          {/* First column with text */}
+          <div className="col">
+            <div className="text">
+              <p>Your text content goes here.</p>
+            </div>
+          </div>
+          
+          {/* Second column with image */}
+          <div className="col">
+            <div className="cdimage">
+              <img
+                src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1730892347/Anuya/Camilla.png"
+                alt="Camilla"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-    );
-}
+    </div>
+  );
+};
+
+
 
 export default Camilla;

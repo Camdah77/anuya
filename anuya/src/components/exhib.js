eb3dcb79a1e6c8321884c5fbaa5ab4176519986f
Camilla Dahlström <136627458+Camdah77@users.noreply.github.com>
2024-12-20 15:14:52 +0000

    table working. Need styuling

--- anuya/src/components/exhib.js
+++ anuya/src/components/exhib.js
@@ -5,7 +5,9 @@ import "../styles/exhib.css";
 function Exhib() {
   return (
     <div>
-    <div className="app-heading">This is my story</div>
+      <div className="app-heading">Exhibitions</div>
+      <div className="table">
+    
 
     <Table striped bordered hover>
       <thead>
@@ -19,123 +21,28 @@ function Exhib() {
         </tr>
       </thead>
       <tbody>
-<tr>
-<td>2018</td>
-<td>2018</td>
-<td>2018</td>
-<td>2018</td>
-<td>2019</td>
-<td>2019</td>
-<td>2019</td>
-<td>2019</td>
-<td>2020</td>
-<td>2021</td>
-<td>2021</td>
-<td>2021</td>
-<td>2021</td>
-<td>2021</td>
-<td>2021</td>
-<td>2022</td>
-<td>2022</td>
-</tr>
-<tr>
-<td>September</td>
-<td>Oktober</td>
-<td>Oktober</td>
-<td>December</td>
-<td>Februari</td>
-<td>Februari</td>
-<td>Mars</td>
-<td>Maj</td>
-<td>Oktober</td>
-<td>April</td>
-<td>April</td>
-<td>April</td>
-<td>Juni</td>
-<td>Juni</td>
-<td>Juli</td>
-<td>Januari</td>
-<td>Maj</td>
-</tr>
-<tr>
-<td>Atelje Öga</td>
-<td>ArtCastle Kristianstad</td>
-<td>Artworks 4 Change</td>
-<td>Galleri Freja</td>
-<td>Stora Brännbo Sigtuna</td>
-<td>Edsviks Art Fair</td>
-<td>Kulturhuset Vallentuna</td>
-<td>Vaxholms vårsalong 2019 Roddarhuset</td>
-<td>Pusch Stockholm, Gallerian</td>
-<td>@glennsportsbargbg</td>
-<td>Vaxholm Hotell</td>
-<td>Galleri Grip</td>
-<td>Vallentuna Kulturhus och Bibliotek</td>
-<td>Galleri Pionen</td>
-<td>Konstlobbyn</td>
-<td>Nacksta Sundsvall!</td>
-<td>Cac Mijas, Spain Costa del sol</td>
-</tr>
-<tr>
-<td>Solo </td>
-<td>collective </td>
-<td>collective</td>
-<td>collective </td>
-<td>Solo </td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>Solo </td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-<td>collective</td>
-</tr>
-<tr>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700243/Anuya/Exhib/180929.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700243/Anuya/Exhib/181028_artcastle.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734702868/Anuya/Exhib/181030_Art4change.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700244/Anuya/Exhib/181201.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700245/Anuya/Exhib/190201.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700246/Anuya/Exhib/190426.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/180330.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700246/Anuya/Exhib/190511.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700247/Anuya/Exhib/201010.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/180929_2.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700249/Anuya/Exhib/210401vaxholmhotell.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700249/Anuya/Exhib/210417.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700251/Anuya/Exhib/210608_VallentunaNy.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700251/Anuya/Exhib/210608_VallentunaNy.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/19KL.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700252/Anuya/Exhib/220122.jpg" alt= "exhibitioncard"/></td>
-<td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700252/Anuya/Exhib/220525_cacmijas.jpg" alt= "exhibitioncard"/></td>
-</tr>
-<tr>
-<td></td>
-<td></td>
-<td>charity fundraising Plan International</td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-<td></td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-<td>Arrange by Konstlobbyn</td>
-</tr>
+<tr><td>2021-2013</td><td>-</td><td>Konstlobbyn, Täby Centrum</td><td>Solo & collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/19KL.jpg"alt= "exhibitioncard"></img></td></tr>
+<tr><td>2022</td><td>January</td><td>Nacksta Sundsvall!</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700252/Anuya/Exhib/220122.jpg" alt= "exhibitioncard" /></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2022</td><td>May</td><td>Cac Mijas, Spain Costa del sol</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700252/Anuya/Exhib/220525_cacmijas.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2021</td><td>April</td><td>@glennsportsbargbg</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/180929_2.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2021</td><td>April</td><td>Vaxholm Hotell</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700249/Anuya/Exhib/210401vaxholmhotell.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2021</td><td>April</td><td>Galleri Grip</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700249/Anuya/Exhib/210417.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2021</td><td>June</td><td>Vallentuna Kulturhus och Bibliotek</td><td>Solo </td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700251/Anuya/Exhib/210608_VallentunaNy.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2021</td><td>June</td><td>Galleri Pionen</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700251/Anuya/Exhib/210608_VallentunaNy.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2021</td><td>July</td><td>Konstlobbyn</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/19KL.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2020</td><td>Octobre</td><td>Pusch Stockholm, Gallerian</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700247/Anuya/Exhib/201010.jpg" alt= "exhibitioncard"/></td><td>Arrange by Konstlobbyn</td></tr>
+<tr><td>2019</td><td>February</td><td>Stora Brännbo Sigtuna</td><td>Solo </td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700245/Anuya/Exhib/190201.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2019</td><td>March</td><td>Kulturhuset Vallentuna</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700242/Anuya/Exhib/180330.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2019</td><td>May</td><td>Vaxholms vårsalong 2019 Roddarhuset</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700246/Anuya/Exhib/190511.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2018</td><td>September</td><td>Atelje Öga</td><td>Solo </td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700243/Anuya/Exhib/180929.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2018</td><td>Octobre</td><td>ArtCastle Kristianstad</td><td>collective </td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700243/Anuya/Exhib/181028_artcastle.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+<tr><td>2018</td><td>Octobre</td><td>Artworks 4 Change</td><td>collective</td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734702868/Anuya/Exhib/181030_Art4change.jpg" alt= "exhibitioncard"/></td><td>charity fundraising Plan International</td></tr>
+<tr><td>2018</td><td>DecDecembre</td><td>Galleri Freja</td><td>collective </td><td><img src="https://res.cloudinary.com/dj9sie6nl/image/upload/v1734700244/Anuya/Exhib/181201.jpg" alt= "exhibitioncard"/></td><td></td></tr>
+
 
       </tbody>
     </Table>
+    </div>
     </div>
   );
 }
